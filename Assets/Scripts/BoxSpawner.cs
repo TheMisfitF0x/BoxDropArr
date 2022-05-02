74a7a51c21177808a353535978484dead63df836
TheMisfitF0x <logan.d.orender@gmail.com>
2022-05-02 17:59:51 -0400

    implement box size randomization

--- Assets/Scripts/BoxSpawner.cs
+++ Assets/Scripts/BoxSpawner.cs
@@ -12,9 +12,22 @@ public class BoxSpawner : MonoBehaviour
     {
         GameObject box_Obj = Instantiate(box_Prefab);
 
+        RandomizeBoxSize(box_Obj);
+
         Vector3 temp = transform.position;
         temp.z = 0f;
 
         box_Obj.transform.position = temp;
+    }
+
+    private void RandomizeBoxSize(GameObject box)
+    {
+        float minSize = .2f;
+        float maxSize = .6f;
+
+        float scaleX = Random.Range(minSize, maxSize);
+        float scaleY = Random.Range(minSize, maxSize);
+
+        box.transform.localScale = new Vector3(scaleX, scaleY, 1);
     }
 }
